7f9465a4173327c337ea2c04fdb4e2aabc3afba3
Hugo <hugot.anglade@gmail.com>
2024-08-14 10:57:57 -0400

    m06 ex01 finished

--- m06/ex01/Serialization.cpp
+++ m06/ex01/Serialization.cpp
@@ -1,7 +1,10 @@
 #include "Serialization.hpp"
 
+Serialization::Serialization(){}
+Serialization::~Serialization(){}
+
 /*
-Reinterpret_cast sert a 
+Reinterpret_cast sert a reinterpr'eter un pointeur attention ce cast ne verifie pas grand chose ce qui a pour consequence 
 */
 
 uintptr_t Serialization::serialize(Data* ptr)
--- m06/ex01/main.cpp
+++ m06/ex01/main.cpp
@@ -1,14 +1,25 @@
 #include "Serialization.hpp"
+#include <iostream>
+#include "Data.hpp" 
+
 
 int main(int ac, char **av)
 {
 	if (ac == 2)
 	{
-		std::string str = av[1];
-		Data data = {str, str.length()};
+		Data data(av[1]);
+		std::cout << "BEFORE serialization " << std::endl; 
+		std::cout << "Data str = " << data << std::endl; 
+
 		uintptr_t ptr = Serialization::serialize(&data);
-		data = Serialization::deserialize(ptr);
-		std::cout << "After deserialization" << std::endl; 
+		data = *Serialization::deserialize(ptr);
+		std::cout << std::endl; 
+		std::cout << "---\n\nAFTER serialization " << std::endl; 
+		std::cout << "Data str = " << data << std::endl; 
+	}
+	else{
+
+		std::cout << "Error: usage: ./serialization <string> " << std::endl; 
 	}
 
 
